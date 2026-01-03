b3de0b8424b72209c69fce66d8a38b79a607431a
perappu <perappu@users.noreply.github.com>
2026-01-03 17:25:58 +0000

    refactor: fix blade formatting

--- resources/views/widgets/_inventory_select.blade.php
+++ resources/views/widgets/_inventory_select.blade.php
@@ -6,7 +6,10 @@
         $categories = \App\Models\Item\ItemCategory::visible(Auth::user() ?? null)->get();
     }
     if (!isset($item_filter)) {
-        $item_filter = \App\Models\Item\Item::orderBy('name')->released()->get()->keyBy('id');
+        $item_filter = \App\Models\Item\Item::orderBy('name')
+            ->released()
+            ->get()
+            ->keyBy('id');
     }
 @endphp
 <h3>
