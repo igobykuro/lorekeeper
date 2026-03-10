d64cf3a397edb8bd3a04e796a7c45cc9b2f48ee7
Kazimir <stokoriel@gmail.com>
2026-03-09 21:08:47 -0500

    fix: typo

--- resources/views/admin/pets/create_edit_drop.blade.php
+++ resources/views/admin/pets/create_edit_drop.blade.php
@@ -69,7 +69,7 @@
     </div>
     <div class="form-group">
         {!! Form::label('cap', 'Drop Cap (Optional)', ['class' => 'form-label ml-3']) !!} {!! add_help('How many batches of drops are allowed to accumulate. Either set to 0 or unset to allow unlimited accumulation.') !!}
-        {!! Form::number('cap', $drop->caps ?? null, ['class' => 'form-control mr-2', 'placeholder' => 'Drop Cap']) !!}
+        {!! Form::number('cap', $drop->cap ?? null, ['class' => 'form-control mr-2', 'placeholder' => 'Drop Cap']) !!}
     </div>
     <div class="form-group">
         {!! Form::checkbox('is_active', 1, $drop->id ? $drop->isActive : 1, ['class' => 'form-check-input', 'data-toggle' => 'toggle']) !!}
